2d1881086d7fcdb2e94ac378d64aab960b01dd40
JiangShen <iplaycloud@gmail.com>
2016-12-29 11:32:19 +0800

    2th init

--- app/src/main/java/com/iplay/jread/about/widget/AboutFragment.java
+++ app/src/main/java/com/iplay/jread/about/widget/AboutFragment.java
@@ -18,6 +18,9 @@ import com.iplay.jread.R;
  */
 public class AboutFragment extends Fragment {
 
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
